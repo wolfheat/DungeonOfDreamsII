1472d8616b48a541674f6c7d9a989a89f74ce924
wolfheat <99273780+wolfheat@users.noreply.github.com>
2025-04-25 04:51:22 +0200

    Doors
    * Animated opening of doors implemented

--- Assets/Scripts/Objects/Door.cs
+++ Assets/Scripts/Objects/Door.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using Wolfheat.StartMenu;
 public class Door : MonoBehaviour
@@ -15,9 +16,51 @@ public class Door : MonoBehaviour
         GetComponent<Collider>().enabled = false;
         SoundMaster.Instance.PlaySound(SoundName.OpenDoor);
 
+        // Animated opening of door
+        StartCoroutine(OpenDoorCO());
+
+        // Instant opening of door
+        //doorPart.transform.Rotate(0, -90, 0);
+    }
+    
+    public void OpenDoorAnimate()
+    {
+        Debug.Log("Animate opening door");
+
+        GetComponent<Collider>().enabled = false;
+        SoundMaster.Instance.PlaySound(SoundName.OpenDoor);
+        StartCoroutine(OpenDoorCO());
         doorPart.transform.Rotate(0, -90, 0);
     }
 
+    private IEnumerator OpenDoorCO()
+    {
+        float startRotation = doorPart.transform.rotation.eulerAngles.y;
+        Quaternion startRotationQuaternion = doorPart.transform.localRotation;
+        float currentRotation = startRotation;
+        //Quaternion endRotationQuaternion = Quaternion.LookRotation(-doorPart.transform.right, Vector3.up);
+        Quaternion endRotationQuaternion = startRotationQuaternion * Quaternion.AngleAxis(-95f, Vector3.up);
+        Quaternion fullyOpenedRotationQuaternion = startRotationQuaternion * Quaternion.AngleAxis(-105f, Vector3.up);
+            //Quaternion.RotateTowards(startRotationQuaternion,endRotationQuaternion,90+15f);
+                    
+        const float RotateTime = 0.2f;
+        float rotateTimer = 0;
 
+        // Opening rotation fast
+        while(rotateTimer < RotateTime) {
+            rotateTimer += Time.deltaTime;
+            doorPart.transform.localRotation = Quaternion.Lerp(startRotationQuaternion,fullyOpenedRotationQuaternion,rotateTimer/RotateTime);
+            yield return null;
+        }
 
+        rotateTimer = 0;
+
+        // Rotate back and stop
+        while(rotateTimer < RotateTime) {
+            rotateTimer += Time.deltaTime;
+            doorPart.transform.localRotation = Quaternion.Lerp(doorPart.transform.localRotation, endRotationQuaternion, 0.1f);
+            yield return null;
+        }
+        doorPart.transform.localRotation = endRotationQuaternion;            
+    }
 }
